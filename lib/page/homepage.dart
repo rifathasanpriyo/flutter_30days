2b0a75928a3b31d23e99565e7625c20b34c720f5
Rifat Hasan Priyo <137903375+rifathosan445@users.noreply.github.com>
2024-01-12 01:27:24 +0600

    day14
    Local Files | Load & Decode JSON

--- lib/page/homepage.dart
+++ lib/page/homepage.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_day2/page/model/catalogs.dart';
 import 'package:flutter_day2/widgets/drawer.dart';
 import 'package:flutter_day2/widgets/item_widget.dart';
 // ignore: camel_case_types
-class homePage extends StatelessWidget {
+class homePage extends StatefulWidget {
    homePage({super.key});
 
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+void initState()
+{
+  super.initState();
+  loadData();
+}
+loadData()async
+{
+  var catalogJson= await rootBundle.loadString("assets/file/catalog.json");
+  var decodeData= jsonDecode(catalogJson);
+  var products= decodeData["products"];
+  print(products);
+}
   @override
   Widget build(BuildContext context) {
      int num=30;
      final dummyList=List.generate(20, (index) => catalogModel.items[0]);
 String name="Flutter";
+
+
+
     return Scaffold(
         appBar: AppBar(
           title: Text("Catalog App",style: TextStyle(color: Colors.black),),
